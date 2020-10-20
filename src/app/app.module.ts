67700c5e5a8a5b062f64fc43146b7b160ad935b1
javibishop <javibishop@gmail.com>
2020-10-20 19:58:32 -0300

    fix import modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,25 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { ErrorInterceptor } from '../app/helpers/error.interceptor';
 import { JwtInterceptor } from '../app/helpers/jwt.interceptor';
 import { AuthGuard } from '../app/helpers/auth.guard';
-
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule,
-         MatListModule, MatFormFieldModule, MatCardModule, MatInputModule,
-         MatRadioModule, MatSelectModule, MatCheckboxModule, MatTableModule, MatMenuModule,
-         MatTabsModule, MatDatepickerModule, MatNativeDateModule,
-         MatGridListModule, MatProgressSpinnerModule,MatDialogModule
-       } from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatTableModule} from '@angular/material/table';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatDialogModule} from '@angular/material/dialog';
 import {MatDialog, MatDialogRef}  from '@angular/material/dialog';     
 
 import { ProfesionalManagerComponent } from './profesional/profesional-manager/profesional-manager.component';
