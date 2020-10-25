b8b03daca6214ffecba80650958ba0d980da17bc
Javier <javibishop@gmail.com>
2020-10-25 15:38:41 -0300

    fixes back url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //baseUrl: 'https://gidasprimvarela.herokuapp.com/'
-  baseUrl: 'http://localhost:3000/'
+  baseUrl: 'https://gidasprimvarela.herokuapp.com/'
+  //baseUrl: 'http://localhost:3000/'
 };
